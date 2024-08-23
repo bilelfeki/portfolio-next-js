c74fa3fa340691ba386889b727eeab6e6eb65d76
ng-bilel <bilel.feki.92@gmail.com>
2024-08-23 21:37:36 +0100

    fix(build): remove unused portfolio page

--- app/about/profile.tsx
+++ app/about/profile.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Button from '../shared/button';
 
 const About = () => {
   return (
--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import Link from "next/link";
 import Button from "../shared/button";
 import Logo from "./logo";
 
 export default function Navbar() {
   const navLinks = [
     { href: "/about", name: "About" },
-    { href: "/profile", name: "Blog" },
+    { href: "/blog", name: "Blog" },
   ];
   return (
     <nav className="flex justify-end">
       <div className="flex justify-end mr-32 mt-10">
         <Logo></Logo>
-        <Button className="" name="About me" link="skills"></Button>
-        <Button className="" name="Skills" link="skills"></Button>
-        <Button className="" name="Portfolio" link="skills"></Button>
+        <Button className="" name="About me" link="about"></Button>
+        <Button className="" name="Skills" link="about"></Button>
+        <Button className="" name="Portfolio" link="about"></Button>
         <Button className="" name="CONTACT ME" link="about"></Button>
       </div>
     </nav>
